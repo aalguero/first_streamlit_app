95ef0af13c158fd71fb77074addb87612a3204fa
Andre Alguero <andre.alguero@gmail.com>
2022-10-18 16:41:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,7 @@ streamlit.dataframe(fruits_to_show)
 
 # new advice section
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.text(fruityvice_normalized)
 streamlit.dataframe(fruityvice_normalized)
 
