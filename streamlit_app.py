325cde22c0f2edb0752b518cb46072821647ddc1
Andre Alguero <andre.alguero@gmail.com>
2022-10-18 16:28:14 -0400

    Update streamlit_app.py
    added api call with package requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import pandas
+import requests
 import streamlit
 
 streamlit.title('Healthy Diner')
@@ -13,3 +14,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
